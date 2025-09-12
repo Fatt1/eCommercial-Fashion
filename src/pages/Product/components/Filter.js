6109feb9018a990af35ab658834732c08468dfd3
Fatt1 <phat3022005@gmail.com>
2025-09-12 18:16:09 +0700

    add html css price-range and material filter

--- src/pages/Product/components/Filter.js
+++ src/pages/Product/components/Filter.js
@@ -181,6 +181,71 @@ export default function Filter() {
             <img src="../assets/dropdown-icon.svg" style="margin-left: 3px" />
           </button>
         </fieldset>
+
+        <fieldset class="filter-group price-range-filter">
+            <legend class="filter-group__header">Khoảng giá</legend>
+            <div class="price-range-filter__edit">
+              <div class="price-range-filter__inputs">
+                  <input class="price-range-filter__input" type="text" maxlength="13" placeholder="đ TỪ">
+                  <div class="price-range-filter__range-line"></div>
+                   <input class="price-range-filter__input" type="text" maxlength="13" placeholder="đ ĐẾN">
+                </div>
+                <button class="btn apply-btn">Áp dụng</button>
+              </div>
+        </fieldset>
+
+         <fieldset class="filter-group material-filter">
+          <legend class="filter-group__header">Chất liệu</legend>
+          <div class="checkbox-filter">
+            <label class="checkbox" for="cotton">
+              <div class="stick-checkbox">
+                <svg
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M11.3872 2.3408L4.82409 10.154L1.23486 6.56476L1.7652 6.03444L4.77597 9.04521L10.8129 1.8584L11.3872 2.3408Z"
+                    fill="black"
+                  />
+                </svg>
+              </div>
+              <input id="cotton" name="cotton" type="checkbox" />
+              <span class="checkbox-label">Cotton</span>
+            </label>
+          </div>
+
+          <div class="checkbox-filter">
+            <label class="checkbox" for="denim">
+              <div class="stick-checkbox">
+                <svg
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M11.3872 2.3408L4.82409 10.154L1.23486 6.56476L1.7652 6.03444L4.77597 9.04521L10.8129 1.8584L11.3872 2.3408Z"
+                    fill="black"
+                  />
+                </svg>
+              </div>
+              <input id="denim" name="denim" type="checkbox" hidden />
+              <span class="checkbox-label">Denim</span>
+            </label>
+          </div>
+          <button class="filter-group__toggle-btn">
+            Xem thêm
+            <img src="../assets/dropdown-icon.svg" style="margin-left: 3px" />
+          </button>
+        </fieldset>
       </div>
   `;
 }
