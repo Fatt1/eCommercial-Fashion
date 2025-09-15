9ffaa12741db98df3acbc8ee833b820a9bdfd3d9
koro <koro140120@gmail.com>
2025-09-15 19:51:51 +0700

    review-section v3

--- src/pages/Home/components/ReviewSection.js
+++ src/pages/Home/components/ReviewSection.js
@@ -2,7 +2,6 @@ export default function ReviewSection() {
   return `
     <div class="review-section">
       <div class="main-content">
-        <div class="review-section-container">
           <h2 class="review-text rt1">
             Hơn 20,000 Doanh nghiệp, cá nhân đã chọn Diorn-shop
           </h2>
@@ -11,38 +10,43 @@ export default function ReviewSection() {
           </h3>
           <div class="review">
             <div class="review-box rvbox1">
+            </br>
+            </br>
               Sau hơn 4 năm sử dụng Diorn-shop với website này, tôi thấy
               Diorn-shop đáp ứng được rất tốt khả năng chăm sóc khách hàng
               online. Hy vọng Diorn sẽ bước tiếp và có những sự đổi mới bất ngờ
               nữa.
               <img
-                src="/eCommercial-Fashion/src/assets/review-icon1.png"
+                src="../assets/review-icon1.png"
                 class="review-icon"
               />
             </div>
             <div class="review-box rvbox2">
+            </br>
+            </br>
               Sau hơn 4 năm sử dụng Diorn-shop với website này, tôi thấy
               Diorn-shop đáp ứng được rất tốt khả năng chăm sóc khách hàng
               online. Hy vọng Diorn sẽ bước tiếp và có những sự đổi mới bất ngờ
               nữa.
               <img
-                src="/eCommercial-Fashion/src/assets/review-icon2.png"
+                src="../assets/review-icon2.png"
                 class="review-icon"
               />
             </div>
             <div class="review-box rvbox3">
+            </br>
+            </br>
               Sau hơn 4 năm sử dụng Diorn-shop với website này, tôi thấy
               Diorn-shop đáp ứng được rất tốt khả năng chăm sóc khách hàng
               online. Hy vọng Diorn sẽ bước tiếp và có những sự đổi mới bất ngờ
               nữa.
               <img
-                src="/eCommercial-Fashion/src/assets/review-icon3.jpg"
+                src="../assets/review-icon3.jpg"
                 class="review-icon"
               />
             </div>
           </div>
         </div>
       </div>
-    </div>
   `;
 }
